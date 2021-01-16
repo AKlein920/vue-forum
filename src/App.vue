0a34171f16e8f7d70fa6b99b499d6899b07adbf7
Abi Klein <abi920@gmail.com>
2021-01-15 23:08:53 -0500

    Dummy data in place; working on styling

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class='container'>
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 
-const name = "abi";
 export default {
   name: "App",
   components: {
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,74 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<div>
+  <div v-for="thread in threads" v-bind:key='thread.threadId' class="col-large push-top">
+    <h1>{{thread.title}}</h1>
+  <div class="post-list">
+  <div v-for="postId in thread.posts" v-bind:key="postId" class='post'>
+    <div class="user-info">
+                      <a href="profile.html#profile-details" class="user-name">{{users[posts[postId].userId].name}}</a>
+
+                      <a href="#">
+                          <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
+                      </a>
+
+                      <p class="desktop-only text-small">107 posts</p>
+
+                      <p class="desktop-only text-small">23 threads</p>
+
+                      <span class="online desktop-only">online</span>
+
+                  </div>
+
+                  <div class="post-content">
+                      <div>
+                        <p>
+                          {{posts[postId].text}}
+                        </p>
+                      </div>
+                      <a href="#" style="margin-left: auto;" class="link-unstyled" title="Make a change"><i class="fa fa-pencil"></i></a>
+                  </div>
+
+                  <div class="post-date text-faded">
+                      {{posts[postId].publishedAt}}
+                  </div>
+
+                  <div class="reactions">
+                      <ul>
+                          <li>💡</li>
+                          <li>❤️</li>
+                          <li>👎</li>
+                          <li>👍</li>
+                          <li>👌</li>
+                      </ul>
+                      <button class="btn-xsmall"><span class="emoji">❤️</span>️ 3</button>
+                      <button class="btn-xsmall active-reaction"><span class="emoji">👌️</span>️ 1</button>
+                      <button class="btn-xsmall">+ <i class="fa fa-smile-o emoji"></i></button>
+                  </div>
+
+  </div>
+
+  </div>
+
+
+  </div>
+
+
   </div>
 </template>
 
 <script>
+import sourceData from "@/data";
+console.log(sourceData);
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  data() {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
